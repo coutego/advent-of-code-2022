2a7c4eea7399c0be835da0ba00a98ddaba49ef69
Coutego <coutego@gmail.com>
2022-12-02 07:20:46 +0100

    day2 python

--- src/aoc22.py
+++ src/aoc22.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Optional, Any
+from typing import Callable, List, Tuple, Optional, Any
 from itertools import zip_longest
 from functools import reduce
 
@@ -89,11 +89,84 @@ def test_d1():
 
 
 def d1p1():
-    return max(calories(read_input_day("d1p1", opt_int)))
+    return max(calories(read_input_day("d1", opt_int)))
 
 
 def d1p2():
-    return sum_top_n(3, calories(read_input_day("d1p1", opt_int)))
+    return sum_top_n(3, calories(read_input_day("d1", opt_int)))
+
+
+# Day 2
+values_option = {"A": 1, "B": 2, "C": 3}
+equivalences = {"X": "A", "Y": "B", "Z": "C"}
+
+
+def result_play(a, b) -> int:
+    if a == b:
+        return 1
+    if a == "A":
+        return 0 if b == "C" else 2
+    if a == "B":
+        return 0 if b == "A" else 2
+    if a == "C":
+        return 0 if b == "B" else 2
+
+
+def score_play(p: Tuple) -> int:
+    (play, reply) = p
+    score_choice = 1
+    if reply == "B":
+        score_choice = 2
+    if reply == "C":
+        score_choice = 3
+    return 3 * result_play(play, reply) + score_choice
+
+
+def score_strategy(st: List) -> int:
+    ret = 0
+    for p in st:
+        ret += score_play(p)
+    return ret
+
+
+def test_d2():
+    assert score_strategy([("A", "B")]) == 8
+    assert score_strategy([("B", "A")]) == 1
+    assert score_strategy([("C", "C")]) == 6
+    assert score_strategy([("A", "B"), ("B", "A"), ("C", "C")]) == 15
+    assert d2p1() == 11767
+    assert d2p2() == 13886
+
+
+def parse_play(s: str) -> Tuple:
+    (p, r) = tuple(s.split())
+    r = equivalences[r]
+    return (p, r)
+
+
+winners = {"A": "B", "B": "C", "C": "A"}
+
+
+def win_lose_to_choice(p: str, r: str) -> str:
+    if r == "Y":
+        return p
+    if r == "Z":
+        return winners[p]
+    return next(k for k, v in winners.items() if v == p)
+
+
+def parse_play2(s: str) -> Tuple:
+    (p, r) = tuple(s.split())
+    r = win_lose_to_choice(p, r)
+    return (p, r)
+
+
+def d2p1():
+    return score_strategy(read_input_day("d2", parse_play))
+
+
+def d2p2():
+    return score_strategy(read_input_day("d2", parse_play2))
 
 
 if __name__ == "__main__":
