4b2e0e3bd22e2f11ad2f64640eca65721e018162
Coutego <coutego@gmail.com>
2022-12-06 16:23:05 +0100

    cut long lines in elisp

--- src/aoc22.el
+++ src/aoc22.el
@@ -314,8 +314,12 @@ It applies 'parse-line-fn' to all lines, if indicated."
 (ert-deftest d06 ()
   "Test d06"
   (should (eq 11 (my-start-of-message 4 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")))
-  (should (eq 1760 (->> (my-read-input-day "d6") car (my-start-of-message 4))))
-  (should (eq 2974 (->> (my-read-input-day "d6") car (my-start-of-message 14)))))
+  (should (eq 1760 (->> (my-read-input-day "d6")
+                        car
+                        (my-start-of-message 4))))
+  (should (eq 2974 (->> (my-read-input-day "d6")
+                        car
+                        (my-start-of-message 14)))))
 
 
 (provide 'aoc22)
